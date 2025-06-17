fb86c2a94e9ec6724ee6d099fef071ce7bdb4766
Samyra752 <costa.santos.samyra@escola.pr.gov.br>
2025-06-17 08:40:48 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  background("#3C88C4");
+  
+   rect(400,0,10,900)
+  text("🌄",450,500,990)
+  text("🏢",-23,500,890)
+    text("🌁",50,500,890)
+
+   
+  textSize(300)
+  
+  
+  
+}
